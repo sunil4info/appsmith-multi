e4547078d8972c778dcd015635fd063e4b6c1c0c
Sunil G <sunil4info@gmail.com>
2024-02-07 11:46:10 +0000

    Remove dashboard page

--- pages/Dashboard/jsobjects/randomID/randomID.js
+++
@@ -1,8 +0,0 @@
-export default {
-	randomSalesID: () => {
-    return Math.ceil(Math.random() * (9999 -  + 1)) + 1;
-},
-	randomCustomerID: () => {
-    return Math.ceil(Math.random() * (99999 -  + 1)) + 1;
-} 
-	}
