9c94a316a7b8b7cb76a5a876c25ea7bf05a67511
Sunil G <sunil4info@gmail.com>
2024-02-13 11:19:45 +0000

    Authentication change

--- pages/Users/jsobjects/EditUserJS/EditUserJS.js
+++ pages/Users/jsobjects/EditUserJS/EditUserJS.js
@@ -1,19 +1,22 @@
 export default {
-
+	refreshAccessToken: () => { 
+		const refresh = appsmith.store.refresh; 
+		const newtoken = refreshtoken.data?.data.access;
+		storeValue("token", newtoken);
+	},
 	editUser () {
-		users.refreshAccessToken();
-
 		Edit_User.run(() =>{
-
 			const responsedata = Edit_User?.data?.data;
 			const data = Edit_User?.data;
 			if(responsedata != null)
 			{
 
 				showAlert(data.message);
+				users.GetUsers();
 				//tblUsers.triggeredRow.firstName = inputFirstName;
 				//tblUsers.triggeredRow.lastName = inputLastName;
-				closeModal('modalEditUser');
+				//closeModal('modalEditUser');
+				//setTimeout()(() => { users.GetUsers() }, 1000);
 				//navigateTo();
 				//setInterval(() => { users.GetUsers(); }, 1000, "myTimer");
 			}
--- pages/Users/jsobjects/users/users.js
+++ pages/Users/jsobjects/users/users.js
@@ -1,16 +1,16 @@
 export default {
-	//refreshAccessToken: () => { 
-	//const refresh = appsmith.store.refresh; 
-	//const newtoken = refreshtoken.data?.data.access;
-	//storeValue("token", newtoken);
-	//},
+	refreshAccessToken: () => { 
+		const refresh = appsmith.store.refresh; 
+		const newtoken = refreshtoken.data?.data.access;
+		storeValue("token", newtoken);
+	},
 	authentication () {
 		var token = appsmith.store.token;
 		if(token === null || token === '' || token === 'undefined'){
 			navigateTo('Login');
 		}
 	},
-	async GetUsers () {
+	GetUsers () {
 		var data =  getUsers.data?.data;
 		if(data != null){
 			return data;
